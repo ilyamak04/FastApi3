b63909723006f1046a4a62e8e37946de2b38aacb
ilyamak04 <makilya04@gmail.com>
2024-06-19 10:36:49 +0300

    users model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy.orm import declared_attr
 
 from app.config import settings
@@ -11,3 +11,10 @@ class Base(DeclarativeBase):
     metadata = MetaData(
         naming_convention=settings.db.naming_convention,
     )
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
