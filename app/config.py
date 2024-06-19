27f14b7fe3433e61eb13558b9383c9aff75f0724
ilyamak04 <makilya04@gmail.com>
2024-06-19 10:37:08 +0300

    add env file

--- app/config.py
+++ app/config.py
@@ -39,7 +39,7 @@ class DatabaseConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=(".env"),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
