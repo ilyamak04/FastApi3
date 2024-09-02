5f41fd1bf55975f99ae85d669ae73e237e409618
ilyamak04 <makilya04@gmail.com>
2024-09-02 14:59:03 +0300

    create user model

---
+++ app/auth/models.py
@@ -0,0 +1,12 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from ..models import Base
+
+
+class User(SQLAlchemyBaseUserTable, Base):
+    __tablename__ = "user"
--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy.orm import declared_attr
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 
 from app.config import settings
 
@@ -11,10 +10,4 @@ class Base(DeclarativeBase):
     metadata = MetaData(
         naming_convention=settings.db.naming_convention,
     )
-
-
-class User(Base):
-    __tablename__ = "user"
-
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
