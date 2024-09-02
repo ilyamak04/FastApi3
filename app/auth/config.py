f3996aa8b0964719366de32a8c33c316e7ee6790
ilyamak04 <makilya04@gmail.com>
2024-09-02 17:53:01 +0300

    relocate AccesToken table

--- app/auth/config.py
+++ app/auth/config.py
@@ -1,33 +1,6 @@
-from typing import TYPE_CHECKING
-
-from fastapi import Depends
 from fastapi_users.authentication import BearerTransport
-from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
-from fastapi_users_db_sqlalchemy.access_token import (
-    SQLAlchemyAccessTokenDatabase,
-    SQLAlchemyBaseAccessTokenTable,
-)
-from sqlalchemy import ForeignKey, Integer
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import Mapped, mapped_column
-
-from ..models import Base
-
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
 
 bearer_transport = BearerTransport(
     # TODO: update url
     tokenUrl="auth/jwt/login"
 )
-
-
-class AccessToken(Base, SQLAlchemyBaseAccessTokenTable):
-    __tablename__ = "access_token"
-    user_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("user.id", ondelete="cascade"), nullable=False
-    )
-
-    @classmethod
-    def get_db(cls, session: AsyncSession):
-        return SQLAlchemyAccessTokenDatabase(session, cls)
--- app/auth/models.py
+++ app/auth/models.py
@@ -2,8 +2,21 @@ from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+from typing import TYPE_CHECKING
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyAccessTokenDatabase,
+    SQLAlchemyBaseAccessTokenTable,
+)
 
 from ..models import Base
 
@@ -14,3 +27,14 @@ class User(Base, SQLAlchemyBaseUserTable):
     @classmethod
     def get_db(cls, session: AsyncSession):
         return SQLAlchemyUserDatabase(session, User)
+
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable):
+    __tablename__ = "access_token"
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("user.id", ondelete="cascade"), nullable=False
+    )
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
