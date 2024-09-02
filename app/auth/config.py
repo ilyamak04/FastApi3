333ab8c2806103b54d9f7e49bc4bc71d27bd01f3
ilyamak04 <makilya04@gmail.com>
2024-09-02 22:31:40 +0300

    create auth backend

--- app/auth/config.py
+++ app/auth/config.py
@@ -1,6 +1,40 @@
-from fastapi_users.authentication import BearerTransport
+from typing import TYPE_CHECKING
+
+from fastapi import Depends
+from fastapi_users.authentication import AuthenticationBackend, BearerTransport
+from fastapi_users.authentication.strategy.db import (
+    AccessTokenDatabase,
+    DatabaseStrategy,
+)
+
+from app.config import settings
+
+from .dependencies import get_access_token_db
+
+if TYPE_CHECKING:
+    from .models import AccessToken
+
 
 bearer_transport = BearerTransport(
     # TODO: update url
     tokenUrl="auth/jwt/login"
+)
+
+SECRET = "SECRET"
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        access_token_db, lifetime_seconds=settings.access_token.lifetime_seconds
+    )
+
+
+auth_backend = AuthenticationBackend(
+    name="access-token-db",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
 )
--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,14 +1,9 @@
 from typing import TYPE_CHECKING
 
 from fastapi import Depends
-from fastapi_users.authentication.strategy.db import (
-    AccessTokenDatabase,
-    DatabaseStrategy,
-)
 from fastapi_users.db import SQLAlchemyUserDatabase
 from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
 
-from app.config import settings
 from app.database import db_helper
 
 from .models import User
@@ -27,11 +22,3 @@ async def get_access_token_db(
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
-
-
-def get_database_strategy(
-    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
-) -> DatabaseStrategy:
-    return DatabaseStrategy(
-        access_token_db, lifetime_seconds=settings.access_token.lifetime_seconds
-    )
