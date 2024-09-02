ce0cd8c4a52b1b08c192aae2ffe72a138070977b
ilyamak04 <makilya04@gmail.com>
2024-09-02 21:53:46 +0300

    add get_user, get_access_token, get_db_strategy deps f

---
+++ app/auth/dependencies.py
@@ -0,0 +1,38 @@
+import uuid
+from typing import TYPE_CHECKING
+
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import (
+    AccessTokenDatabase,
+    DatabaseStrategy,
+)
+from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
+
+from app.config import settings
+from app.database import db_helper
+
+from .models import User
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+    from .models import AccessToken
+
+
+async def get_user_db(session: AsyncSession = Depends(db_helper.session_getter)):
+    yield SQLAlchemyUserDatabase(session, User)
+
+
+async def get_access_token_db(
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        access_token_db, lifetime_seconds=settings.access_token.lifetime_seconds
+    )
