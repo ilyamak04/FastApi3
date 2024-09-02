74aec3d971cd59c39d97cff005670b226f189368
ilyamak04 <makilya04@gmail.com>
2024-09-02 17:49:37 +0300

    delete useless imports

--- app/auth/models.py
+++ app/auth/models.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
 from ..models import Base
