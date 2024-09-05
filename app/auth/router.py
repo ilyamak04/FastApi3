e41e1891488a2ac771cf9601323ba756baf7e70c
ilyamak04 <makilya04@gmail.com>
2024-09-05 19:15:03 +0300

    include auth router

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,5 +1,7 @@
+from fastapi import APIRouter
 from fastapi_users import FastAPIUsers
 
+from ..config import settings
 from .config import auth_backend
 from .dependencies import get_user_manager
 from .models import User
@@ -7,4 +9,10 @@ from .models import User
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
     [auth_backend],
+)
+
+router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
 )
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from fastapi import APIRouter
-from app.config import settings
+
+from fastapi import APIRouter, FastAPI
 from fastapi.responses import ORJSONResponse
 
+from app.config import settings
 from app.database import db_helper
+
+from .auth.router import router as auth_router
 
 
 @asynccontextmanager
@@ -16,6 +18,7 @@ async def lifespan(app: FastAPI):
 
 
 router = APIRouter()
+
 app = FastAPI(
     lifespan=lifespan,
     default_response_class=ORJSONResponse,
@@ -25,3 +28,4 @@ app.include_router(
     router,
     prefix=settings.api.prefix,
 )
+router.include_router(router=auth_router)
