d8abe91b8764d71e7b8bf8a2c10aad3aa9022c84
ilyamak04 <makilya04@gmail.com>
2024-09-05 17:51:39 +0300

    add depend ger_user_manager

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -7,6 +7,7 @@ from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDataba
 from app.database import db_helper
 
 from .models import User
+from .service import UserManager
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -22,3 +23,7 @@ async def get_access_token_db(
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
