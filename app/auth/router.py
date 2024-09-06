ddbb9a538097303c45866967f7007b614054d513
ilyamak04 <makilya04@gmail.com>
2024-09-06 15:58:55 +0300

    add users router

--- app/auth/router.py
+++ app/auth/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi_users import FastAPIUsers
 
 from app.auth.models import User
-from app.auth.schemas import UserCreate, UserRead
+from app.auth.schemas import UserCreate, UserRead, UserUpdate
 from app.config import settings
 
 from .config import auth_backend
@@ -12,7 +12,7 @@ fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
     [auth_backend],
 )
-
+users_router = APIRouter(prefix=settings.api.v1.users)
 auth_router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
 auth_router.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -20,3 +20,4 @@ auth_router.include_router(
 auth_router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
 )
+users_router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate))
--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import APIRouter, FastAPI
 from fastapi.responses import ORJSONResponse
 
-from app.auth.router import auth_router
+from app.auth.router import auth_router, users_router
 
 from .config import settings
 from .database import db_helper
@@ -24,5 +24,7 @@ app = FastAPI(
 main_router = APIRouter()
 
 main_router.include_router(auth_router)
+main_router.include_router(users_router)
+
 
 app.include_router(main_router, prefix=settings.api.prefix_v1)
